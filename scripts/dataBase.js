39e608f1ec2021ac52706393e71ac8eeb98bb236
Antonio <netoifpe@gmail.com>
2022-10-22 16:49:18 -0300

    correção de endereço de imgs

--- scripts/dataBase.js
+++ scripts/dataBase.js
@@ -3,38 +3,38 @@ export const date = [
         name: "Tanya Sinclair",
         bio: "UX Engineer",
         depoiment: '“ I’ve been interested in coding for a while but never taken the jump, until now. I couldn’t recommend this course enough. I’m now in the job of my dreams and so excited about the future. ”',
-        img: "../assets/img/image-tanya.jpg"
+        img: "./assets/img/image-tanya.jpg"
     },
 
     {
         name: "John Tarkpor",
         bio: "Junior Front-end Developer",
         depoiment: '“ If you want to lay the best foundation possible I’d recommend taking this course.The depth the instructors go into is incredible. I now feel so confident about starting up as a professional developer. ”',
-        img: "../assets/img/image-john.jpg"
+        img: "./assets/img/image-john.jpg"
     },
     {
         name: "Random Person 1",
         bio: "Random Bio 1",
         depoiment: '"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore mgna aliqua. Accumsan tortor posuere ac ut. Matis molestie a iaculis at erat."',
-        img: "../assets/img/image-person-1.jpg"
+        img: "./assets/img/image-person-1.jpg"
     },
     {
         name: "Random Person 2",
         bio: "Random Bio 2",
         depoiment: '"Lorem ipsum volutpat aenean tristique tempor sit rhoncus vitae mi, habitasse mauris habitant dui cras lobortis dolor habitasse, facilisis placerat interdum sit dui semper integer nam." ',
-        img: "../assets/img/image-person-2.jpg"
+        img: "./assets/img/image-person-2.jpg"
     },
     {
         name: "Random Person 5",
         bio: "Random Bio 5",
         depoiment: '"Lorem ipsum venenatis nullam nam fermentum interdum ante pellentesque ultrices tempus volutpat condimentum, aenean cras eu torquent ultricies metus conubia ornare molestie consequat orci."',
-        img: "../assets/img/image-person-5.jpg"
+        img: "./assets/img/image-person-5.jpg"
     },
     {
         name: "Random Person 6",
         bio: "Random Bio 6",
         depoiment: '"Lorem ipsum potenti convallis libero platea hendrerit, maecenas tristique massa tincidunt ut ipsum id, sit amet porta suscipit vehicula. aenean rhoncus tortor justo nullam sagittis at aenean ultrices"',
-        img: "../assets/img/image-person-6.jpg"
+        img: "./assets/img/image-person-6.jpg"
     }
 ]
 
